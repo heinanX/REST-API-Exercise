afc209f97ee05324c563d862c098d48be9c8dd1f
heinanX <larrii@live.se>
2022-12-29 10:46:50 +0100

    fixed get data

--- app.js
+++ app.js
@@ -5,11 +5,10 @@ let fs = require('fs')
 app.use(express.json())
 
 app.get('/songs', (req, res) =>
-    res.status(200).send(
-        /* "Here are my songs" */
-        fs.readFile('songs.json', 'utf8', (err, data) => {
-            console.log(data.toString())
-        })
-))
+
+    fs.readFile('songs.json', 'utf8', (err, data) => {
+        res.status(200).send(data)
+    })
+)
 
 app.listen(2800, () => console. log('server is up localhost:2800/songs'));
