ca7cd703abd497e233cd5076070505013d336bb5
heinanX <larrii@live.se>
2022-12-30 22:04:09 +0100

    Added GET

--- server.js
+++ server.js
@@ -5,3 +5,16 @@ const fs = require('fs');
 
 
 app.listen(3000, () => {console.log('Servern är igång. Eller...')})
+
+app.get('/songs', (req, res) => {
+
+    fs.readFile('songs.json', (err, data) => {
+       if (err) {
+        console.log('error 404')
+       }
+
+       const songs = JSON.parse(data)
+       res.send(songs)
+       return;
+    })
+})
